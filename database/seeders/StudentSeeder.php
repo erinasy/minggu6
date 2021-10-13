15f1afec66df474b45e86795790860306b7fba37
erinasy <erinaseviyanti02@gmail.com>
2021-10-13 19:40:35 +0700

    pembuatan tabel students

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('students')->insert([
+            'nim'=>'2031710012',
+            'name' => 'Erina Seviyanti',
+            'class' => 'MI2E',
+            'departement' => 'MI',
+            'phone_number'  => '087739772017',
+        ]);
+        DB::table('students')->insert([
+            'nim'=>'2031710017',
+            'name' => 'Eva Monika S',
+            'class' => 'MI2E',
+            'departement' => 'MI',
+            'phone_number'  => '087621782918',
+        ]);
+        DB::table('students')->insert([
+            'nim'=>'2031710074',
+            'name' => 'Fariz Al Baihaqqi',
+            'class' => 'MI2E',
+            'departement' => 'MI',
+            'phone_number'  => '085627189201',
+        ]);
+    }
+}
